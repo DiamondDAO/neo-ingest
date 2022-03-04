928397ae056d01d12a468b9a5f76027a74d07cde
Rohan Bansal <robansal@stanford.edu>
2022-03-03 21:09:46 -0800

    Add relationships

---
+++ generate-daohaus-relationships.py
@@ -0,0 +1,164 @@
+import boto3
+from datetime import datetime
+import json
+from neo4j import GraphDatabase
+import pandas as pd
+import s3fs
+from dotenv import load_dotenv
+from helpers.cypher import (
+    create_dao_relationships,
+    create_approved_token_relationships,
+    create_member_relationships,
+    create_proposal_relationships,
+    create_vote_relationships,
+)
+from helpers.s3 import write_df_to_s3, set_object_private, read_df_from_s3
+from helpers.proposals import label_desc, label_proposal_type, label_status, label_title
+import os
+
+
+class ChainverseGraph:
+    def __init__(self, uri, user, password):
+        self.__driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.__driver.close()
+
+    def query(self, query, parameters=None, db=None):
+        assert self.__driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try:
+            session = self.__driver.session(database=db) if db is not None else self.__driver.session()
+            response = list(session.run(query, parameters))
+        except Exception as e:
+            print("Query failed:", e)
+        finally:
+            if session is not None:
+                session.close()
+        return response
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    uri = os.getenv("NEO_URI")
+    username = os.getenv("NEO_USERNAME")
+    password = os.getenv("NEO_PASSWORD")
+    conn = ChainverseGraph(uri, username, password)
+
+    resource = boto3.resource("s3")
+    s3 = boto3.client("s3")
+    BUCKET = "chainverse"
+
+    # read in raw csvs from s3
+    member_df = read_df_from_s3(BUCKET, "neo/daohaus/raw/member.csv")
+    token_df = read_df_from_s3(BUCKET, "neo/daohaus/raw/token.csv")
+    proposal_df = read_df_from_s3(BUCKET, "neo/daohaus/raw/proposal.csv")
+    dao_df = read_df_from_s3(BUCKET, "neo/daohaus/raw/dao.csv")
+
+    dao_json = dao_df.to_dict("records")
+    primary_dao_list = []
+    approved_token_list = []
+    for entry in dao_json:
+        current_dict = {}
+        current_dict["dao"] = entry["id"]
+
+        try:
+            current_dict["summoner"] = str(entry["summoner"])
+        except:
+            current_dict["summoner"] = ""
+
+        try:
+            current_dict["depositToken"] = entry["depositToken"]["id"].split("-")[2]
+        except:
+            current_dict["depositToken"] = ""
+
+        try:
+            approvedTokens = entry["approvedTokens"].replace("'", '"')
+            approvedTokens = json.loads(approvedTokens)
+        except:
+            approvedTokens = []
+
+        for token in approvedTokens:
+            try:
+                approved_token_list.append({"dao": current_dict["dao"], "approvedToken": token["id"].split("-")[2]})
+            except:
+                print("failed approved token")
+                continue
+
+        primary_dao_list.append(current_dict)
+
+    # create dao primary relationships
+    primary_dao_relationship_df = pd.DataFrame(primary_dao_list)
+    url = write_df_to_s3(primary_dao_relationship_df, BUCKET, "neo/daohaus/relationships/dao.csv", resource, s3)
+    create_dao_relationships(url, conn)
+    set_object_private(BUCKET, "neo/daohaus/relationships/dao.csv", resource)
+
+    # create approved token relationships
+    approved_token_relationship_df = pd.DataFrame(approved_token_list)
+    url = write_df_to_s3(
+        approved_token_relationship_df, BUCKET, "neo/daohaus/relationships/approved_token.csv", resource, s3
+    )
+    create_approved_token_relationships(url, conn)
+    set_object_private(BUCKET, "neo/daohaus/relationships/approved_token.csv", resource)
+
+    member_json = member_df.to_dict("records")
+    member_dao_list = []
+    for entry in member_json:
+        dao_id = entry["id"].split("-")[0]
+        member_address = entry["id"].split("-")[2]
+        member_dao_list.append({"dao": dao_id, "member": member_address})
+
+    # create primary member relationships
+    member_dao_df = pd.DataFrame(member_dao_list)
+    url = write_df_to_s3(member_dao_df, BUCKET, "neo/daohaus/relationships/member.csv", resource, s3)
+    create_member_relationships(url, conn)
+    set_object_private(BUCKET, "neo/daohaus/relationships/member.csv", resource)
+
+    proposal_json = proposal_df.to_dict("records")
+    proposal_dao_list = []
+    vote_list = []
+    for entry in proposal_json:
+        current_dict = {}
+        current_dict["dao"] = entry["molochAddress"]
+        current_dict["applicant"] = entry["applicant"]
+        current_dict["sponsor"] = entry["sponsor"]
+        current_dict["proposer"] = entry["proposer"]
+        current_dict["processor"] = entry["processor"]
+        current_dict["proposal"] = entry["id"]
+
+        try:
+            fixedVotes = entry["votes"].replace("'", '"')
+            fixedVotes = json.loads(fixedVotes)
+        except:
+            fixedVotes = []
+
+        for vote in fixedVotes:
+            try:
+                vote_dict = {}
+                vote_dict["dao"] = current_dict["dao"]
+                vote_dict["proposal"] = current_dict["proposal"]
+                vote_dict["createdAt"] = vote["createdAt"]
+                vote_dict["vote"] = vote["uintVote"]
+                vote_dict["member"] = vote["id"].split("-")[2]
+                vote_list.append(vote_dict)
+
+            except:
+                print("failed vote")
+                continue
+
+        proposal_dao_list.append(current_dict)
+
+    proposal_dao_df = pd.DataFrame(proposal_dao_list)
+    url = write_df_to_s3(proposal_dao_df, BUCKET, "neo/daohaus/relationships/proposal.csv", resource, s3)
+    create_proposal_relationships(url, conn)
+    set_object_private(BUCKET, "neo/daohaus/relationships/proposal.csv", resource)
+
+    vote_rel_df = pd.DataFrame(vote_list)
+    url = write_df_to_s3(vote_rel_df, BUCKET, "neo/daohaus/relationships/votes.csv", resource, s3)
+    create_vote_relationships(url, conn)
+    set_object_private(BUCKET, "neo/daohaus/relationships/votes.csv", resource)
+
+    conn.close()
+
--- helpers/cypher.py
+++ helpers/cypher.py
@@ -1,52 +1,117 @@
 def create_token_nodes(url, conn):
-    
-    unique_query = '''CREATE CONSTRAINT UniqueTokenAddress IF NOT EXISTS FOR (d:Token) REQUIRE d.address IS UNIQUE'''
-    
+
+    unique_query = """CREATE CONSTRAINT UniqueTokenAddress IF NOT EXISTS FOR (d:Token) REQUIRE d.address IS UNIQUE"""
+
     conn.query(unique_query)
-    
-    token_node_query = f'''
+
+    token_node_query = f"""
                         LOAD CSV WITH HEADERS FROM '{url}' AS tokens
                         MERGE(t:Token {{address: tokens.address}})
                         set t = tokens
-                    '''
+                    """
 
     conn.query(token_node_query)
     print("token nodes created")
-    
+
+
 def create_member_nodes(url, conn):
-    
-    member_node_query = f'''
+
+    member_node_query = f"""
                         LOAD CSV WITH HEADERS FROM '{url}' AS members
                         MERGE(t:Member:Daohaus {{address: members.address, daoAddress: members.daoAddress}})
                         set t = members
-                    '''
+                    """
     conn.query(member_node_query)
     print("member nodes created")
-    
+
+
 def create_dao_nodes(url, conn):
-    
-    unique_query = '''CREATE CONSTRAINT UniqueAddressId IF NOT EXISTS FOR (d:Dao) REQUIRE d.address IS UNIQUE'''
-    
+
+    unique_query = """CREATE CONSTRAINT UniqueAddressId IF NOT EXISTS FOR (d:Dao) REQUIRE d.address IS UNIQUE"""
+
     conn.query(unique_query)
-    
-    dao_node_query = f'''
+
+    dao_node_query = f"""
                         LOAD CSV WITH HEADERS FROM '{url}' AS daos
                         MERGE(d:Dao:Daohaus:Entity {{address: daos.address}})
                         set d = daos
-                    '''
+                    """
     conn.query(dao_node_query)
     print("dao nodes created")
-    
+
+
 def create_proposal_nodes(url, conn):
-    
-    unique_query = '''CREATE CONSTRAINT UniqueProposalId IF NOT EXISTS FOR (d:Proposal) REQUIRE d.id IS UNIQUE'''
-    
+
+    unique_query = """CREATE CONSTRAINT UniqueProposalId IF NOT EXISTS FOR (d:Proposal) REQUIRE d.id IS UNIQUE"""
+
     conn.query(unique_query)
-    
-    proposal_node_query = f'''
+
+    proposal_node_query = f"""
                         LOAD CSV WITH HEADERS FROM '{url}' AS proposals
                         MERGE(d:Proposal:Daohaus {{id: proposals.id}})
                         set d = proposals
-                    '''
+                    """
     conn.query(proposal_node_query)
     print("proposal nodes created")
+
+
+def create_dao_relationships(url, conn):
+    relationship_query = f"""
+                            LOAD CSV WITH HEADERS FROM '{url}' AS row
+                            MATCH (dao:Daohaus:Dao {{address: row.dao}}), (summoner:Daohaus:Member {{address: row.summoner, daoAddress: row.dao}}), (t:Token {{address: row.depositToken}})
+                            MERGE (dao)-[:HAS_DEPOSIT_TOKEN]->(t)
+                            MERGE (summoner)-[:IS_SUMMONER]->(dao)
+                            
+                            """
+
+    conn.query(relationship_query)
+    print("dao relationships created")
+
+
+def create_approved_token_relationships(url, conn):
+    relationship_query = f"""
+                         LOAD CSV WITH HEADERS FROM '{url}' as row
+                         MATCH (dao:Daohaus:Dao {{address: row.dao}}), (t:Token {{address: row.approvedToken}})
+                         MERGE (dao)-[:HAS_APPROVED_TOKEN]->(t)
+                        """
+
+    conn.query(relationship_query)
+    print("approved token relationships created")
+
+
+def create_member_relationships(url, conn):
+    relationship_query = f"""
+                         LOAD CSV WITH HEADERS FROM '{url}' as row
+                         MATCH (d:Daohaus:Dao {{address: row.dao}}), (m:Daohaus:Member {{address: row.member, daoAddress: row.dao}})
+                         MERGE (m)-[r:IS_MEMBER]->(d)
+                        """
+
+    conn.query(relationship_query)
+    print("member relationships created")
+
+
+def create_proposal_relationships(url, conn):
+    relationship_query = f"""
+                         LOAD CSV WITH HEADERS FROM '{url}' as row
+                         MATCH (p:Daohaus:Proposal {{id: row.proposal}}), (app:Daohaus:Member {{address: row.applicant, daoAddress: row.dao}}), (prop:Daohaus:Member {{address: row.proposer, daoAddress: row.dao}}),
+                         (spon:Daohaus:Member {{address: row.sponsor, daoAddress: row.dao}}), (proc:Daohaus:Member {{address: row.processor, daoAddress: row.dao}}), (d:Daohaus:Dao {{address: row.dao}})
+                         MERGE (d)-[:HAS_PROPOSAL]->(p)
+                         MERGE (spon)-[:SPONSORED]->(p)
+                         MERGE (prop)-[:PROPOSED]->(p)
+                         MERGE (proc)-[:PROCESSED]->(p)
+                         MERGE (app)-[:IS_APPLICANT]->(p)
+                        """
+
+    conn.query(relationship_query)
+    print("proposal relationships created")
+
+
+def create_vote_relationships(url, conn):
+    relationship_query = f"""
+                        LOAD CSV WITH HEADERS FROM '{url}' as row
+                        MATCH (p:Daohaus:Proposal {{id: row.propsal}}), (voter:Daohaus:Member {{address: row.member, daoAddress: row.dao}})
+                        MERGE (voter)-[:VOTED {{createdAt: row.createdAt, vote: row.vote}}]->(p)
+                        """
+
+    conn.query(relationship_query)
+    print("vote relationships created")
